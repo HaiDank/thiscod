54cacd98f1fca060c30645cb531bc48aa7234003
HaiDank <132982748+HaiDank@users.noreply.github.com>
2025-06-26 17:37:53 +0700

    add logout button (#13)

---
+++ components/log-out-button.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+async function signOut() {
+    await authStore.signOut();
+}
+</script>
+
+<template>
+    <UButton
+        class="w-full"
+        color="error"
+        variant="ghost"
+        trailing-icon="material-symbols:logout"
+        @click="signOut"
+    >
+        Log out
+    </UButton>
+</template>
