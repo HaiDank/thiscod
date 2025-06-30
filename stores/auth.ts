2e9a6210e49dc2e2185ed95e39da11048a3bfe7a
HaiDank <danevil2003@gmail.com>
2025-06-30 13:54:31 +0700

    add ssr authentication

--- stores/auth.ts
+++ stores/auth.ts
@@ -4,9 +4,14 @@ import { defineStore } from "pinia";
 const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("auth", () => {
-    const session = authClient.useSession();
-    const user = computed(() => session.value.data?.user);
-    const loading = computed(() => session.value.isPending || session.value.isRefetching);
+    const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(null);
+    async function init() {
+        const data = await authClient.useSession(useFetch);
+        session.value = data;
+    }
+
+    const user = computed(() => session.value?.data?.user);
+    const loading = computed(() => session.value?.isPending);
 
     async function signInWithGithub() {
         await authClient.signIn.social({
@@ -42,6 +47,7 @@ export const useAuthStore = defineStore("auth", () => {
     }
 
     return {
+        init,
         loading,
         signInWithGithub,
         signInWithEmail,
