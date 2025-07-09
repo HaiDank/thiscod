fdbc0aecfd5d2c51d34af4096af7ff8485eeda74
HaiDank <132982748+HaiDank@users.noreply.github.com>
2025-07-09 10:06:48 +0700

    add chip status (#39)

--- components/app/user-util-bar.vue
+++ components/app/user-util-bar.vue
@@ -1,41 +1,10 @@
 <script setup lang="ts">
-import { cn } from "~/lib/utils";
 
-const authStore = useAuthStore();
 </script>
 
 <template>
     <div class="sticky rounded-lg bg-card w-full h-full flex items-center px-2 border border-background shadow-md gap-1">
-        <UButton
-            v-if="authStore.user"
-            variant="ghost"
-            class="p-0 rounded-tl-2xl rounded-bl-2xl group rounded-tr-md rounded-br-md  grow-1"
-            color="neutral"
-        >
-            <UChip
-                inset
-                position="bottom-right"
-                color="success"
-            >
-                <UAvatar
-                    v-if="authStore.user"
-                    :src="authStore.user.image ?? undefined"
-                    :alt="authStore.user.name"
-                    class="size-10"
-                />
-            </UChip>
-            <p class="relative flex flex-col items-baseline justify-baseline overflow-hidden w-full">
-                <span class="font-semibold text-foreground">
-                    {{ authStore.user.name }}
-                </span>
-                <span
-                    :class="cn('flex flex-col transition-all ease-in-out group-hover:-translate-y-full w-full')"
-                >
-                    <span class="text-disabled scale-y-100 flex items-baseline transition-all group-hover:scale-y-0  text-xs opacity-100 hover:opacity-0 truncate max-w-full">online</span>
-                    <span class="absolute top-full text-disabled text-xs truncate max-w-full">{{ authStore.user.email }}</span>
-                </span>
-            </p>
-        </UButton>
+        <AppAvatarChip />
         <UTooltip
             text="Mute"
             :delay-duration="0"
